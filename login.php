1f39a2922ca44521a3e31ae19200dc537a3b2490
OumarVincent <oumar.sylla75018@gmail.com>
2024-02-26 13:04:42 +0100

    Modification mineure de la page de connexion

--- login.php
+++ login.php
@@ -94,7 +94,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <h2 class="text-center text-dark mt-5">Bienvenue !</h2>
-                <div class="text-center mb-5 text-dark">Connectez vous</div>
+                <!--<div class="text-center mb-5 text-dark">Connectez vous</div>-->
                 <div class="card my-5">
 
                     <form action ="login.php" method="POST" class="card-body cardbody-color p-lg-5">
@@ -112,10 +112,6 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                         </div>
                         <div class="text-center"><button type="submit"
                                 class="btn btn-color px-5 mb-5 w-100">Login</button></div>
-                        <div id="emailHelp" class="form-text text-center mb-5 text-dark">Not
-                            Registered? <a href="#" class="text-dark fw-bold"> Create an
-                                Account</a>
-                        </div>
                     </form>
                 </div>
 
